6874e76b129500ba14bac0f4d4e3cdf431249958
Michael Nzube <michaelnzube@Michaels-MacBook-Pro.local>
2019-08-13 16:56:11 +0200

    Commit message ch(react setup): implement react setup with webpack - added react , babel and webpack packages - Added webpack configuration
    [(Starts ##166790041]

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ah-jawans-frontend
+Authors Haven 
+
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello Jawans , We are up and ready to go!!!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
+
+
