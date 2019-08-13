4709305866d9a2175be4a0bcd4d2c4c202835054
Michael Nzube <michaelnzube@Michaels-MacBook-Pro.local>
2019-08-13 15:16:52 +0200

    Commit message ch(Sets up React): implement react AH project with webpack - added react, webpack and babel packages - Added webpack , css and babel configurations
    [(Starts #166790041]

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello Jawans , We are up and ready to go!!!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
