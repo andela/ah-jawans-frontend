454928e2b856a0069ab60974d692105ccd0d6d41
kagabof <faustinkagabo@gmail.com>
2019-08-14 09:56:11 +0200

    ch(redux setup): Add redux setup
    - Add component folder
    - Added homePage component for test
    - Added redux action, reducers and configureStore.
    
    [delivers #166790042]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import { render } from "react-dom";
+import { BrowserRouter as Router } from 'react-router-dom'
+import configureStore from './redux/configureStore';
+import { Provider } from 'react-redux';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const store = configureStore();
+render(
+  <Provider store={store} >
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+);
